ccc8c99bd089a0403c18f078fa5532a7271ab262
moha08 <moha.ajori@gmail.com>
2019-09-28 19:05:31 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -6,10 +6,28 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  
+  @override
+  State<StatefulWidget> createState() {
+    
+    return MyAppState();
+  }
 
+}
+class MyAppState extends State<MyApp>{
+
+  var questionIndex = 0;
+  var questionList = [
+    'What is you name?',
+    'What is your favourite colour?'
+  ];
   void answerQuestion(){
-    print("Question Answers!!!");
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    
+    print(questionList[questionIndex]);
   }
   @override
   Widget build(BuildContext context) {
@@ -21,7 +39,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text("What is your name"),
+            Text(questionList[questionIndex]),
             RaisedButton(
               child: Text("Mohammad"),
               onPressed: answerQuestion,
