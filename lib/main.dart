ebdbf8fb1e6a860c2b099c33d6dd92f64145ccde
moha08 <moha.ajori@gmail.com>
2019-09-29 13:15:13 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './question.dart';
 
 /* void main() {
   runApp(MyApp());
@@ -7,39 +8,34 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatefulWidget {
-  
   @override
   State<StatefulWidget> createState() {
-    
     return MyAppState();
   }
-
 }
-class MyAppState extends State<MyApp>{
 
+class MyAppState extends State<MyApp> {
   var questionIndex = 0;
-  var questionList = [
-    'What is you name?',
-    'What is your favourite colour?'
-  ];
-  void answerQuestion(){
+  var questionList = ['What is you name?', 'What is your favourite colour?'];
+  void answerQuestion() {
     setState(() {
       questionIndex = questionIndex + 1;
     });
-    
+
     print(questionList[questionIndex]);
   }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My First Application"),
-          backgroundColor: Colors.red[200],
+          backgroundColor: Colors.blue,
         ),
         body: Column(
           children: [
-            Text(questionList[questionIndex]),
+            Question(questionList[questionIndex]),
             RaisedButton(
               child: Text("Mohammad"),
               onPressed: answerQuestion,
